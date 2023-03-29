1308a95a2bd523bf7a212746a82ef489b39e72e2
Ehtisham Mushtaq <ehtishammushtaq@gmail.com>
2023-03-29 13:18:15 -0500

    Some changes to transitions and other changes
    Now when you click an object, it gets enlarged at the same time others get restored to their original scale

--- Scripts/BunnyHeadspace.gd
+++ Scripts/BunnyHeadspace.gd
@@ -1,16 +1,17 @@
 extends Control
 
-@onready var clock: TextureRect = $Clock
+@onready var Clock: TextureRect = $Clock
 @onready var Newspaper: TextureRect = $Newspaper
 @onready var Letter: TextureRect = $Letter
 var focused = false
 
 
 
+
 #$Background, $Clock, $Newspaper, $Letter, $TreasureChest
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var original_scale = clock.scale
+	pass
 	
 #func _clear_zoom(focused_input, tween_name)
 	#pass
@@ -18,33 +19,23 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
 
-func _on_clock_entered(area):
-	if area.get_parent() == Newspaper:
-		clock.hide()
-
-func _on_Newspaper_area_entered(area):
-	if area.get_parent() == clock:
-		Newspaper.hide()
 	
 func _input(event):
+	var TW = create_tween().set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_IN_OUT)
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		if clock.get_global_rect().has_point(event.position):
-			#_on_other_object_pressed()
-			var clock_tween = create_tween()
-			clock_tween.tween_property(clock, "scale", Vector2(1.5, 1.5), 0.8 )
-			#clock_tween.tween_callback(clock.set.bind(clock.Label,"I am a clock"))
-			focused = true
+		if Clock.get_global_rect().has_point(event.position):
+			TW.tween_property(Clock, "scale", Vector2(2.5, 2.5), 0.8 )
+			TW.parallel().tween_property(Newspaper, "scale", Vector2(1, 1), 0.5 )
+			TW.parallel().tween_property(Letter, "scale", Vector2(1, 1), 0.5 )
 		if Newspaper.get_global_rect().has_point(event.position):
-			#_on_other_object_pressed()
-			var newspaper_tween = create_tween()
-			newspaper_tween.tween_property(Newspaper, "scale", Vector2(1.5, 1.5), 0.8 )
-			newspaper_tween.tween_callback(Newspaper.set.bind(Newspaper,"I am a Newspaper"))
-			
+			TW.tween_property(Newspaper, "scale", Vector2(2.5, 2.5), 0.8 )
+			TW.parallel().tween_property(Clock, "scale", Vector2(1, 1), 0.5 )
+			TW.parallel().tween_property(Letter, "scale", Vector2(1, 1), 0.5 )
 		if Letter.get_global_rect().has_point(event.position):
-			#_on_other_object_pressed()
-			var letter_tween = create_tween()
-			letter_tween.tween_property(Letter, "scale", Vector2(1.5, 1.5), 0.8 )
-			letter_tween.tween_callback(Letter.set.bind(Letter,"I am a Letter"))
-
-
+			TW.tween_property(Letter, "scale", Vector2(2.5, 2.5), 0.8 )
+			TW.parallel().tween_property(Newspaper, "scale", Vector2(1, 1), 0.5 )
+			TW.parallel().tween_property(Clock, "scale", Vector2(1, 1), 0.5 )
+			
+			
