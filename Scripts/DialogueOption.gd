19b5fef0649bec2972c3dc6c63d87a638ae37eb9
FreemanSpray <89803643+FreemanSpray@users.noreply.github.com>
2023-04-13 22:36:12 -0500

    Correct mouse hover

--- Scripts/DialogueOption.gd
+++ Scripts/DialogueOption.gd
@@ -33,18 +33,18 @@ func _on_button_pressed():
 			get_parent().get_node("MainScene").continueGame();
 
 
-func _on_left_texture_mouse_entered():
+func _on_left_button_mouse_entered():
 	$LeftOpt/Texture.hide()
 	$LeftOpt/HoverTexture.show()
 	
-func _on_left_hover_texture_mouse_exited():
+func _on_left_button_mouse_exited():
 	$LeftOpt/HoverTexture.hide()
 	$LeftOpt/Texture.show()
 
-func _on_right_texture_mouse_entered():
+func _on_right_button_mouse_entered():
 	$RightOpt/Texture.hide()
 	$RightOpt/HoverTexture.show()
 	
-func _on_right_hover_texture_mouse_exited():
+func _on_right_button_mouse_exited():
 	$RightOpt/HoverTexture.hide()
 	$RightOpt/Texture.show()
